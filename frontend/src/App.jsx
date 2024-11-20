c53d1379b4331cf533d2a78b8eb8b66566540fc6
Aastha Gupta <aastha0904gupta@gmail.com>
2024-11-20 16:56:53 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import LandingPage from "./pages/LandingPage";
-import AdoptPage from "./pages/AdoptPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import AdoptPage from "./pages/AdoptPage/AdoptPage";
 import { pets } from "./data/pets";
 import Footer from "./components/Footer/Footer";
 import PetDescription from '../src/pages/PetDescription/petDescription';
@@ -17,8 +17,6 @@ const App = () => {
         <Route path="/" element={<LandingPage pets={pets} />} />
         <Route path="/adopt" element={<AdoptPage pets={pets} />} />
         <Route path="/petDescription" element={<PetDescription/>} />
-
-
       </Routes>
       </main>
       <Footer/>
